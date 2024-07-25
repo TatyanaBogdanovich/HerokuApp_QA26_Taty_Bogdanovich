c6d278f7eaf5287363abc41334bec8e60d8a2e95
TatyanaBogdanovich <tianabahdanovich@gmail.com>
2024-07-26 00:40:31 +0300

    1

--- src/test/java/tests/InputsTest.java
+++ src/test/java/tests/InputsTest.java
@@ -9,21 +9,26 @@ import org.testng.annotations.Test;
 public class InputsTest extends BaseTest {
 
     @Test
-    public void inputsTest(){
+    public void inputsTest()  {
+
 
         driver.get("https://the-internet.herokuapp.com/inputs");
         WebElement input = driver.findElement(By.cssSelector("input[type=number]"));
         Assert.assertEquals(input.getAttribute("value"), "");
 
         input.sendKeys("qwerty");
-        Assert.assertEquals(input.getAttribute("value"),"", "Ввод нецифрового значения");
+        Assert.assertEquals(input.getAttribute("value"), "");
 
-        input.sendKeys("12345");
-        Assert.assertEquals(input.getAttribute("value"), "12345", "Ввод цифрового значения");
+        input.clear();
 
-        input.sendKeys(Keys.ARROW_UP);
-        input.sendKeys(Keys.ARROW_UP);
+        input.sendKeys("100");
+        Assert.assertEquals(input.getAttribute("value"), "100");
+
         input.sendKeys(Keys.ARROW_DOWN);
-        Assert.assertEquals(input.getAttribute("value"), "123");
-  }
+        input.sendKeys(Keys.ARROW_DOWN);
+        input.sendKeys(Keys.ARROW_DOWN);
+        input.sendKeys(Keys.ARROW_UP);
+        input.sendKeys(Keys.ARROW_UP);
+        Assert.assertEquals(input.getAttribute("value"), "99");
+    }
 }
